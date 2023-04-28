dec869c2e4a46f7cd16f19a4509b9cb804d0c357
aleksandramosyagina <a.mosyagina@list.ru>
2023-04-28 19:59:41 +0300

    feat: add modal window (Fri, Apr 28, 2023, 20:00:00 PM)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-$(document).ready(function() {
-    $('.nav__hamburger').click(function(event){
+$(document).ready(function () {
+    $('.nav__hamburger').click(function (event) {
         $('.nav__hamburger, .nav__menu-mobile, .nav__hamburger_line').toggleClass('active');
         $('body').toggleClass('lock');
     });
 });
-$('.nav__menu-mobile_item-link').click(function(){
+$('.nav__menu-mobile_item-link').click(function () {
     $('.nav__hamburger, .nav__menu-mobile, .nav__hamburger_line').removeClass('active');
     $('body.lock').removeClass('lock');
 });
 
+const buttonElem = document.getElementById("popup-button");
 
+buttonElem.onclick = function () {
+    $('.popup').addClass('active');
+    $('body').toggleClass('lock');
+}
 
+$(document).click(function (e) {
+    if ($(e.target).is('.popup')) {
+        closeModal();
+    }
+})
 
+function closeModal() {
+    $('.popup.active').removeClass('active');
+}
